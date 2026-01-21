d75af226ff819df9b40a70aaa792102744385135
Shubhamrk10-JS <shubhamrk999@gmail.com>
2026-01-21 13:58:36 +0530

    Sonar test

--- TestSonar.java
+++ TestSonar.java
@@ -1,31 +1,11 @@
-public class TestSonar {
-
-    // Code smell: unused variable
-    private static int unusedValue = 10;
+public class HelloWorld {
 
     public static void main(String[] args) {
-
-        // Bug: possible NullPointerException
-        String message = null;
-
-        if (message.equals("Hello")) {
-            System.out.println("Hello World");
-        }
-
-        // Code smell: duplicated logic
-        printNumber(5);
-        printNumber(5);
-
-        // Security hotspot: hardcoded password
-        String password = "admin123";
-        System.out.println("Password is: " + password);
+        HelloWorld hw = new HelloWorld();
+        hw.sayHello();
     }
 
-    private static void printNumber(int number) {
-        if (number > 0) {
-            System.out.println("Positive number");
-        } else {
-            System.out.println("Non-positive number");
-        }
+    public void sayHello() {
+        System.out.println("Hello SonarQube");
     }
 }
